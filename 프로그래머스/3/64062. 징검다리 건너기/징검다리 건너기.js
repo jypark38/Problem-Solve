1cedb20248a174660f20cd45b8d72d809543419c
jypark38 <126536351+jypark38@users.noreply.github.com>
2023-11-23 23:59:57 +0900

    [level 3] Title: 징검다리 건너기, Time: 21.59 ms, Memory: 41.4 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
@@ -1,23 +1,25 @@
 function solution(stones, k) {
-    var answer = binarySearch(stones,k,1,200000000);
-
-    return answer;
+    var answer = []
+    let start=0, end =200000000
+    return bs(start,end,stones,k)
 }
 
-function binarySearch(arr,k,start,end){
-    if(start === end){
-        return start
-    }
+function bs(start,end,stones,k){
+    //종료조건
+    if(start==end) return start
     let mid = parseInt((start+end)/2)
-    let cnt = 0;
-    for(let i=0;i<arr.length;i++){
-        if(cnt===k) break
-        let value = arr[i] - mid
-        value <= 0 ? cnt++ : cnt=0;
+    let cnt = 0
+    for(i=0;i<stones.length;i++){
+        if(stones[i]<=mid){
+            cnt++
+        }else{
+            cnt=0
+        }
+        if(cnt>=k) break
     }
     if(cnt===k){
-        return binarySearch(arr,k,start,mid)
+        return bs(start,mid,stones,k)
     }else{
-        return binarySearch(arr,k,mid+1,end)
-    }   
+        return bs(mid+1,end,stones,k)
+    }
 }
