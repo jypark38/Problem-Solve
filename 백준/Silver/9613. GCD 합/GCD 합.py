ed52dfeb5ff34277006e892f748bdc20af425403
jypark38 <126536351+jypark38@users.noreply.github.com>
2023-11-27 20:06:55 +0900

    [Silver IV] Title: GCD 합, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.py
+++ 백준/Silver/9613. GCD 합/GCD 합.py
@@ -1,13 +1,14 @@
 import math
+import itertools as t
 
 T = int(input())
 
 for i in range(T):
     inp = list(map(int,input().split()))
-    n = inp[0]
-    inp = inp[1:]
-    s = list()
-    for j in range(n):
-        for k in range(j+1,n):
-            s.append(math.gcd(inp[j],inp[k]))
-    print(sum(s))
+    comb = t.combinations(inp[1:],2)
+    answer = 0
+    
+    for (a,b) in comb:
+        answer += math.gcd(a,b)
+    print(answer)
+        
