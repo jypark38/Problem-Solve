7eac911a3b7f2a0ed25bcefdaa4ff7e6201223cf
jypark38 <126536351+jypark38@users.noreply.github.com>
2023-12-04 21:30:18 +0900

    [level unrated] Title: 옹알이 (1), Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.py
@@ -0,0 +1,12 @@
+def solution(babbling):
+    answer = 0
+    arr = ["aya", "ye", "woo", "ma"]
+    
+    for c in babbling:
+        _c = c
+        for u in arr:
+            _c = _c.replace(u,'_')
+        
+        if len(_c.replace('_','')) == 0: answer +=1
+    
+    return answer
