eb4456047925733bbab14cd4a7d0938886d72225
jypark38 <126536351+jypark38@users.noreply.github.com>
2023-12-04 21:31:07 +0900

    [level unrated] Title: 옹알이 (1), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.py
@@ -5,7 +5,7 @@ def solution(babbling):
     for c in babbling:
         _c = c
         for u in arr:
-            _c = _c.replace(u,'_')
+            _c = _c.replace(u,'_',1)
         
         if len(_c.replace('_','')) == 0: answer +=1
     
