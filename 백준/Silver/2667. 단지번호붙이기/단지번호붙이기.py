0aa491bacaf897be681096e9ac406d5714e88506
jypark38 <126536351+jypark38@users.noreply.github.com>
2024-01-15 14:14:10 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31196 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,34 +1,34 @@
-def dfs(x,y):
-    global tmp
-    if x<=-1 or x>=n or y <= -1 or y>= n:
+n = int(input())
+graph = []
+
+for _ in range(n):
+    graph.append(list(map(int,list(input()))))
+
+result = []
+
+def dfs(graph,x,y):
+    global cnt
+    if x < 0 or x >= n:
+        return False
+    if y < 0 or y >= n:
         return False
     if graph[x][y] == 1:
-        graph[x][y]=0
-        tmp += 1
-        dfs(x-1,y)
-        dfs(x+1,y)
-        dfs(x,y-1)
-        dfs(x,y+1)
+        graph[x][y] = 0
+        cnt += 1
+        dfs(graph,x-1,y)
+        dfs(graph,x+1,y)
+        dfs(graph,x,y-1)
+        dfs(graph,x,y+1)
         return True
     return False
 
-n = int(input())
-
-graph = []
-for i in range(n):
-    graph.append(list(map(int,list(input()))))
-
-result1 = 0
-result2 = []
-
 for i in range(n):
     for j in range(n):
-        tmp = 0
-        if dfs(i,j):
-            result1+=1
-            result2.append(tmp)
-result2.sort()
+        cnt = 0
+        if (dfs(graph,i,j)):
+            result.append(cnt)
 
-print(result1)
-for i in result2:
+result.sort()
+print(len(result))
+for i in result:
     print(i)
