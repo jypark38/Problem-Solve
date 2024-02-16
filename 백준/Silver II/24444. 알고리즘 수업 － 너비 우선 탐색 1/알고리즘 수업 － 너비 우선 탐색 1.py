c2620b3eeb83309139276df6aa85a9766bc6d94e
jypark38 <126536351+jypark38@users.noreply.github.com>
2024-02-16 14:44:22 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 672 ms, Memory: 59772 KB -BaekjoonHub

---
+++ 백준/Silver II/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,28 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+n, m, r = map(int,input().split())
+visited = [0 for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
+cnt = 0
+
+for i in range(m):
+    n1,n2 = map(int,input().split())
+    graph[n1].append(n2)
+    graph[n2].append(n1)
+
+q = deque([r])
+
+while q:
+    node = q.popleft()
+    if visited[node]:
+        continue
+    cnt+=1
+    visited[node] = cnt
+    for next in sorted(graph[node],reverse=False):
+        if visited[next] == 0:
+            q.append(next)
+
+for i in visited[1:]:
+    print(i)
