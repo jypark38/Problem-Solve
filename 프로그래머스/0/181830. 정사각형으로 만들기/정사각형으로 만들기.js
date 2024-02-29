2897031cb49c7d059fffe91f00d1baa9bbefcabb
jypark38 <126536351+jypark38@users.noreply.github.com>
2024-02-29 17:22:08 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.05 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -0,0 +1,17 @@
+function solution(arr) {
+    const row = arr.length
+    const col = arr[0].length
+    if(row==col){
+        return arr
+    }
+    if(row<col){
+        const newRow = Array.from({length:col}).fill(0)
+        for(let i = row; i<col;i++) arr.push(newRow)
+        return arr
+    }
+    const answer = arr.map(e=>{
+        for(let i = col;i<row;i++) e.push(0)
+        return e
+    })
+    return answer;
+}
