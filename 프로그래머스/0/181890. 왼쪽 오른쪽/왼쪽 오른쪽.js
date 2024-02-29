8b479097ce508fb7dfd20558767bab17a12a14c6
jypark38 <126536351+jypark38@users.noreply.github.com>
2024-02-29 20:25:44 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,25 +1,12 @@
 function solution(str_list) {
     var answer = [];
-    let l,r
     for(let i=0;i<str_list.length;i++){
         if(str_list[i]=='l'){
-            l=i
-            break
+            return str_list.slice(0,i)
         }
         if(str_list[i]=='r'){
-            r=i
-            break
+            return str_list.slice(i+1,str_list.length)
         }
-    }
-    if(l!=undefined){
-        return str_list.slice(0,l)
-    }
-    if(r!=undefined){
-        return str_list.slice(r+1,str_list.length)
     }
     return [];
 }
-
-console.log(solution(['r','b','b','b']))
-// console.log(solution(['a','a','l']))
-// console.log(solution(['a','a','b','b','b']))
