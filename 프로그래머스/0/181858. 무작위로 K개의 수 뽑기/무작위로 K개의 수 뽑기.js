6d9eb9e259d0763ca204de4daf1f90af5bd56392
jypark38 <126536351+jypark38@users.noreply.github.com>
2024-02-29 17:13:39 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 0.82 ms, Memory: 34.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -0,0 +1,12 @@
+function solution(arr, k) {
+    var answer = [];
+    for(let i=0;i<arr.length;i++){
+        if (answer.length === k) break;
+        if (answer.includes(arr[i])) continue
+        answer.push(arr[i])
+    }
+    while (answer.length !== k){
+        answer.push(-1)
+    }
+    return answer;
+}
