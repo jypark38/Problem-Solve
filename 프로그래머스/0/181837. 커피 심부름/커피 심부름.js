5470a5b223e9a9e5c0c39a32eac26d076f6be556
jypark38 <126536351+jypark38@users.noreply.github.com>
2024-02-29 19:54:05 +0900

    [level 0] Title: 커피 심부름, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
@@ -0,0 +1,22 @@
+function solution(order) {
+    var answer = 0;
+    const price={
+        "americano":4500,
+        "iceamericano":4500, 
+        "americanoice":4500,
+        "hotamericano":4500, 
+        "americanohot":4500,
+        "cafelatte" :5000,
+        "icecafelatte":5000, 
+        "cafelatteice":5000,
+        "hotcafelatte":5000, 
+        "cafelattehot":5000,
+        "anything":4500
+    }
+    
+    for(o of order){
+        answer+=price[o]
+    }
+    
+    return answer;
+}
