62d7cf54c04e776dc8a29ba0ec613cb284865980
jypark38 <126536351+jypark38@users.noreply.github.com>
2024-02-29 19:38:23 +0900

    [level 0] Title: 세 개의 구분자, Time: 34.00 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,17 +1,8 @@
 function solution(myStr) {
     var answer = [];
-    let str = ''
-    for(c of myStr){
-        if(c=='a'||c=='b'||c=='c'){
-            if(str!=''){
-                answer.push(str)
-                str=''
-            }
-        } else{
-            str+=c
-        }
-    }
-    if(str!='') answer.push(str)
-    if(answer.length==0) answer.push('EMPTY')
-    return answer;
+    myStr=myStr.replaceAll('a','-')
+    myStr=myStr.replaceAll('b','-')
+    myStr=myStr.replaceAll('c','-')
+    answer = myStr.split('-').filter(e=>e.length)
+    return answer.length ? answer : ['EMPTY'];
 }
