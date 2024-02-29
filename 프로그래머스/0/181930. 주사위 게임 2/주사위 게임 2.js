9170c905985d1d6f9132ded14bccdc0b47caca01
jypark38 <126536351+jypark38@users.noreply.github.com>
2024-02-29 18:36:59 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,11 +1,11 @@
 function solution(a, b, c) {
     var answer = 0;
-    if(a!=b && b!=c && a!=c){
-        return a+b+c
+    const set = new Set([a,b,c])
+    if(set.size==3){
+        return (a+b+c)
     }
-    if((a==b && b!=c) || (a!=b && b==c) || (a==c && a!=b)){
+    if(set.size==2){
         return (a+b+c)*(a**2+b**2+c**2)
     }
-    
-    return (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3);
+    return (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3)
 }
