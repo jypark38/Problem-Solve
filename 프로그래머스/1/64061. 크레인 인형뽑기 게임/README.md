67ed84e820dac61a5d3b02f8909ed82226557dfe
jypark38 <126536351+jypark38@users.noreply.github.com>
2024-03-01 18:50:27 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.90 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크레인 인형뽑기 게임 - 64061 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.80 ms
+메모리: 33.5 MB, 시간: 0.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 16:34:6
+2024년 03월 01일 18:50:26
 
 ### 문제 설명
 
--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,32 +1,24 @@
 function solution(board, moves) {
     var answer = 0;
-    const stack = []
-    const n = board.length
-    zip= (rows) => rows[0].map((_,c)=>rows.map(row=>row[c]))
-    
-    const tBoard = zip(board)
-    
-    
-    for(i=0;i<moves.length;i++){
-        const pos = moves[i]
-        let curr
-        for(j=0;j<n;j++){
-            if(tBoard[pos-1][j] === 0) continue
-            curr = tBoard[pos-1][j]
-            break;
-        }
-        if(!curr) continue
-        tBoard[pos-1][j] = 0
-        if(stack.length !== 0){
-            if(stack[stack.length-1] === curr) {
-                stack.length -= 1
-                answer+=2
-                continue
+    const stack = [-1]
+    const N = board.length
+    let endS = 0
+    for(m of moves){
+        let cr = m-1
+        for(let i=0;i<N;i++){
+            if(board[i][cr] != 0){
+                stack.push(board[i][cr])
+                board[i][cr] = 0
+                endS++
+                break
             }
         }
-        stack.push(curr)
+        if(stack[endS] == stack[endS-1]){
+            stack.pop()
+            stack.pop()
+            answer+=2
+            endS-=2
+        }
     }
-    
-    
     return answer;
 }
