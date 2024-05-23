a28b4fad853184545d325e57356c591c8e26129e
jypark38 <126536351+jypark38@users.noreply.github.com>
2024-05-23 16:15:16 +0900

    Time: 54 ms (31.82%), Space: 16.8 MB (91.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,28 +1,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        h_s = {}
-        h_t = {}
+        h = {}
         
         if(len(s) != len(t)):
             return False
         
         for i in range(len(s)):
-            c_s = s[i]
-            c_t = t[i]
-            
-            if(c_s not in h_s):
-                h_s[c_s] = 1
+            c = s[i]
+            if(c not in h):
+                h[c] = 1
             else:
-                h_s[c_s] += 1
-            if(c_t not in h_t):
-                h_t[c_t] = 1
+                h[c] += 1
+        
+        for c in t:
+            if(c not in h):
+                return False
             else:
-                h_t[c_t] += 1
-            
-        for c_s in h_s:
-            if(c_s not in h_t):
-                return False
-            elif (h_s[c_s] != h_t[c_s]):
-                return False
-            
+                h[c] -= 1
+                if(h[c] < 0):
+                    return False
+                
         return True
